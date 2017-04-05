66f3a58ca86928542409d9f3c4ff9a88c38e95c5
marcelo <marcelo.araujo1296@gmail.com>
2017-04-05 14:41:18 -0300

    First Files

---
+++ trabalho1/asm/img.asm
@@ -0,0 +1,55 @@
+#=============================================================================#
+#          Trabalho 1 - Organizacao e arquitetura de Computadores             #
+#              Marcelo de Araujo Lopes Junior - 150016794                     #
+#               Pedro Henrique Pires Egler - 150000000                        #
+#                     Luiza Amanajas - 150000000                              #
+#=============================================================================#
+
+
+# DOCUMENTACAO DA ROTINA
+#
+#
+#
+#
+#
+get_point:
+
+
+
+# DOCUMENTACAO DA ROTINA
+#
+#
+#
+#
+#
+draw_point:
+
+
+
+# DOCUMENTACAO DA ROTINA
+#
+#
+#
+#
+#
+draw_empty_rectangle:
+
+
+
+# DOCUMENTACAO DA ROTINA
+#
+#
+#
+#
+#
+convert_negative:
+
+
+
+# DOCUMENTACAO DA ROTINA
+#
+#
+#
+#
+#
+load_image:
