39ed2fb5aa7f2f298443770e28e4f41894cba872
marcelo <marcelo.araujo1296@gmail.com>
2017-04-20 15:23:24 -0300

    MIPS.CPP added

---
+++ trabalho2/src/mips.cpp
@@ -0,0 +1,31 @@
+/* ************************************************* *
+ *  implementacao das funcoes descritas no mips.h    *
+ * ************************************************* */
+
+void fetch(){
+
+}
+
+void decode(){
+
+}
+
+void execute(){
+
+}
+
+void step(){
+
+}
+
+void run(){
+
+}
+
+void dump_mem(int start, int end, char format){
+
+}
+
+void dump_reg(char format){
+
+}
