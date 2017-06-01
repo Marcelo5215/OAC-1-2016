44354f79c001650cc4bb9022ea65ce06288c3f8e
marcelo <marcelo.araujo1296@gmail.com>
2017-06-01 16:49:54 -0300

    Banco de Registradores/ Comentarios

--- trabalho3/src-temp/REG_BANK.vhd
+++ trabalho3/src-temp/REG_BANK.vhd
@@ -1,6 +1,14 @@
+--	Banco de Registradores
+--		clk 	-> clock
+-- 	raddX -> read address(registrador a ser lido)
+--		wren	->	enable para escrita dos registradores
+--		wadd	->	write address (registrador da escrita)
+--		rdataX	->	valor lido do registrador como saida
+
 library	ieee;	
 use	ieee.std_logic_1164.all;	
-use	ieee.numeric_std.all;	
+use	ieee.numeric_std.all;
+use ieee.math_real.all;	
 	
 entity REG_BANK is	
 	
@@ -15,8 +23,29 @@ end entity REG_BANK;
 
 architecture REG_BANK_arch of REG_BANK is
 
+	type	bank is array(NATURAL range <>) of std_logic_vector(DATA_WIDTH-1	downto 0);
+	signal reg	:	bank((2**ADDRESS_WIDTH)-1 to 0);
+	signal zero_add	:	std_logic_vector(ADDRESS_WIDTH-1	downto 0);
+	signal zero	:	std_logic_vector(DATA_WIDTH-1	downto 0);
+
 begin
 
+	zero_add <= std_logic_vector(to_unsigned(0,(DATA_WIDTH)));
+	zero <= std_logic_vector(to_unsigned(0,(ADDRESS_WIDTH)));
+	reg(0) <= zero;
+	
+	process(rising_edge(clk))
+	
+	begin
+	
+		if(wren = '1' and wadd /= zero_add) then 
+			reg(to_integer(unsigned(wadd))) <= wdata;
+		end if;
+	
+		rdata1 <= reg(to_integer(unsigned(radd1)));
+		rdata2 <= reg(to_integer(unsigned(radd2)));
+	
+	end process;
 
 
 
