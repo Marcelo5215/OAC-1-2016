ffff26804f118f6c8cc1b6c70c499883bd5b9d2e
marcelo <marcelo.araujo1296@gmail.com>
2017-06-24 15:04:43 -0300

    Adicionadas .vhd das unidades de controle, CU está incompleta, e ambas nao implementadas ainda

---
+++ Final/CULA.vhd
@@ -0,0 +1,19 @@
+--
+-- Unidade de Controle da ULA
+--		Utiliza o OpCode da unidade de controle e o campo Funct da intrucao
+--		para determinar qual operaao  a ula ira realizar
+--
+
+entity CULA is
+	port( Op	:	in std_logic_vector(1 downto 0);
+			funct	: in std_logic_vector(5 downto 0);
+			ulaop	:	out std_logic_vector(3 downto 0);
+	);
+end entity;
+
+architecture CULA_ARCH of CULA is
+
+begin
+
+
+end CULA_ARCH;
