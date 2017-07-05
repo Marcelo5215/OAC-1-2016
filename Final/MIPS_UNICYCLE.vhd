ec1bf947c077dde856ad696c34b2416773da5ff5
marcelo <marcelo.araujo1296@gmail.com>
2017-07-05 17:56:12 -0300

    Pronto

--- Final/MIPS_UNICYCLE.vhd
+++ Final/MIPS_UNICYCLE.vhd
@@ -8,15 +8,12 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_arith.all;
-use ieee.numeric_std.all;
-use ieee.math_real.all;
-
 
 entity MIPS_UNICYCLE is
 	generic(DATA_WIDTH	:	natural	:=	32;
 			  ADDRESS_WIDTH:	natural	:=	5;
 			  ADDN : natural := 7);
-	port(clk : std_logic			--clock externo
+	port(clkIN : std_logic			--clock externo
 	);
 end entity;
 
@@ -114,7 +111,7 @@ architecture MIPS_UNICYCLE_ARCH of MIPS_UNICYCLE is
 		);
 	end component;
 	
-	signal PC : std_logic_vector(ADDN-1 downto 0) := std_logic_vector(to_unsigned(0,(ADDN)));           -- "Resgistador PC"
+	signal PC : std_logic_vector(ADDN-1 downto 0) := "0000000";           -- "Resgistador PC"
 	signal inst : std_logic_vector(DATA_WIDTH-1 downto 0);   -- Instrucao
 	signal Rdata1 : std_logic_vector(DATA_WIDTH-1 downto 0); -- Valor dentro do registador1 (RS)
 	signal Rdata2 : std_logic_vector(DATA_WIDTH-1 downto 0); -- Valor dentro do registador2 (RT)
@@ -128,17 +125,19 @@ architecture MIPS_UNICYCLE_ARCH of MIPS_UNICYCLE is
 	signal BEQBNE : std_logic;											-- Branch ou nao a pertir do zero da ula
 	signal SaidaMUXBRANCH : std_logic_vector(ADDN-1 downto 0);
 	signal SaidaMUXJUMP : std_logic_vector(ADDN-1 downto 0);
-	signal SaidaMUXJAL1 : std_logic_vector(5 downto 0); -- endreco do registrador de escrita
+	signal SaidaMUXJAL1 : std_logic_vector(ADDRESS_WIDTH-1 downto 0); -- endreco do registrador de escrita
 	signal SaidaMUXJAL2 : std_logic_vector(DATA_WIDTH-1 downto 0); -- dado da escrita no reg
-	signal SaidaMUXRegDst : std_logic_vector(5 downto 0); -- escolha do registrador de escrita
+	signal SaidaMUXRegDst : std_logic_vector(ADDRESS_WIDTH-1 downto 0); -- escolha do registrador de escrita
 	signal SaidaMUXULA : std_logic_vector(DATA_WIDTH-1 downto 0);
 	signal SaidaMUXVOLTA : std_logic_vector(DATA_WIDTH-1 downto 0);
 	signal ADDPC8: std_logic_vector(DATA_WIDTH-1 downto 0);
+	signal DATA8 : std_logic_vector(DATA_WIDTH-1 downto 0) := X"00000008";
+	signal clk : std_logic;
 	
 	--SINAIS DE CONTROLE
 	signal regDst	:	std_logic;
 	signal Branch : std_logic;
-	signal LeMem : std_logic;
+	--signal LeMem : std_logic;
 	signal MemparaReg : std_logic;
 	signal OpALU : std_logic_vector(1 downto 0);
 	signal EscreveMem : std_logic;
@@ -149,18 +148,21 @@ architecture MIPS_UNICYCLE_ARCH of MIPS_UNICYCLE is
 	signal JR : std_logic;	
 	
 begin
-
-	--UNIDADE DE CONTROLE AINDA FALTANDO
-
+	clk <= clkIN;
+	--UNIDADE DE CONTROLE
+	
+	CUU : CU port map(inst(31 downto 26), regDst, Branch, open, MemparaReg,
+							OpALU, EscreveMem, OrigALU, EscreveReg, Jump, Jal, JR);
 
 	InstMemo : MI port map(PC, inst);
 	
 	--MUX para o banco de registradores 
+	--MUXREGDST : MUX5 port map(inst(15 downto 0), inst(20 downto 16), regDst, SaidaMUXRegDst);
 	MUXREGDST : MUX5 port map(inst(15 downto 11), inst(20 downto 16), regDst, SaidaMUXRegDst);
 	MUXJAL1 : MUX5 port map("11111", SaidaMUXRegDst, Jal, SaidaMUXJAL1);
 
 	--Banco de Registradores
-	RegBank : REG_BANK port map(clk, EscreveReg, inst(25 downto 0), inst(20 downto 16), saidaMUXJAL1, SaidaMUXJAL2, Rdata1, Rdata2);
+	RegBank : REG_BANK port map(clk, EscreveReg, inst(25 downto 21), inst(20 downto 16), saidaMUXJAL1, SaidaMUXJAL2, Rdata1, Rdata2);
 	
 	--Extended offset
 	EXTE : SIGN_EX port map(inst(15 downto 0), extended);
@@ -174,20 +176,20 @@ begin
 	ULAA : ULA port map(Rdata1, SaidaMUXULA, ulaop, ULARes, zero, open, open, open);
 	
 	--memoria de dados
-	DataMemo : MD port map(clk, LeMem, ULARes, Rdata2, MDout);
+	DataMemo : MD port map(clk, EscreveMem, ULARes(ADDN-1 downto 0), Rdata2, MDout);
 	
 	--MUX entre ULA E MD
 	MUXVOLTA : MUX port map(MDout, UlaRes, memparaReg, SaidaMUXVOLTA);
-	ADDPC8 <= to_stdlogicvector(unsigned(PC) + to_unsigned(8, DATA_WIDTH-1));
-	MUXJAL2 : MUX port map(ADDPC8, SaidaMUXVOLTA, Jal);
+	ADDPC8 <= (unsigned(X"000000" & "0" & PC) + unsigned(DATA8));
+	MUXJAL2 : MUX port map(ADDPC8, SaidaMUXVOLTA, Jal, SaidaMUXJAL2);
 
-	ADDPC : SOM port map(PC, "00100", PC4);
-	ADDBRANCH : SOM port map(PC4, extended sll 2, SUMBRANCH);	
+	ADDPC : SOM port map(PC, "0000100", PC4);
+	ADDBRANCH : SOM port map(PC4, extended(ADDN-3 downto 0) & "00" , SUMBRANCH);	
 	--Verifica se deve fazer o branch ou nao
 	BEQBNE <= (Branch and not(zero)) or (Branch and zero);
 	MUXBRANCH : MUX7 port map(SUMBRANCH, PC4, BEQBNE, SaidaMUXBRANCH);
 	
-	MUXJUMP : MUX7 port map(PC(ADDN-1 downto ADDN) & (inst(25 downto 0) sll 2), SaidaMUXBRANCH, Jump, SaidaMUXJUMP);
+	MUXJUMP : MUX7 port map(PC(ADDN-1 downto ADDN) & (inst(ADDN-3 downto 0) & "00" ), SaidaMUXBRANCH, Jump, SaidaMUXJUMP);
 
 	MUXPC : MUX7 port map(Rdata1(ADDN-1 downto 0), SaidaMUXJUMP, JR, PC);
 	
