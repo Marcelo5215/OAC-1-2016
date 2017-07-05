950b849dea5dc7b694f8a6ab5662017615c42b60
AnaPergentino <anacpergentino@hotmail.com>
2017-07-05 13:50:58 -0300

    proposta de soculção Controle de ULA e unidade de controle

--- Final/CULA.vhd
+++ Final/CULA.vhd
@@ -10,13 +10,34 @@ use ieee.std_logic_arith.all;
 entity CULA is
 	port( Op	:	in std_logic_vector(1 downto 0);
 			funct	: in std_logic_vector(5 downto 0);
-			ulaop	:	out std_logic_vector(3 downto 0)
+			ctrula	:	out std_logic_vector(3 downto 0)
 	);
 end entity;
 
 architecture CULA_ARCH of CULA is
 
 begin
-
+	
+	begin
+	process (Op,funct)
+	variable maskfunct : std_logic_VECTOR (5 downto 0);
+	begin
+	maskfunct := funct and "001111";
+	--case (Op)
+		if (Op = "00") then
+			ctrula <= ULA_ADD; 
+		elsif (Op = "01") then
+			ctrula <= ULA_SUB; 
+		elsif (Op = "10") then
+			case(maskfunct) is
+				when "000000" => ctrula <= ULA_ADD;
+				when "000010" => ctrula <= ULA_SUB;
+				when "000100" => ctrula <= ULA_AND;
+				when "000101" => ctrula <= ULA_OR;
+				when "001010" => ctrula <= ULA_SLT;
+				when others => ctrula <= ULA_UKW;
+			end case;
+		end if;
+end process;
 
 end CULA_ARCH;
